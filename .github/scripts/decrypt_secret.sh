592d05b90b83198bba79b146d8030931d9883ddc
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-16 09:19:02 +0200

    fix file path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GIT_SECRET" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/secrets/my_secret.json $HOME/secrets/my_secret.json.gpg
